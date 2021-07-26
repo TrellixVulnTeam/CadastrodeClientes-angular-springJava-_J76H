7752d634f4f3bc05f01b71f8a7ea3d7fd490ff80
Gilson José <80110990+gilson789qwe@users.noreply.github.com>
2021-07-26 12:04:48 -0300

    Version 1.0
    Criação do Frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Cadastro de clientes';
-
-  constructor(private http: HttpClient){
-
-  }
-
-  ngOnInit(){
-        
-  }
+export class AppComponent {
+  title = 'frontend';
 }
