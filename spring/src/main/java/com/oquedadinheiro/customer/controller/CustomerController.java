a02a2b1b977b935de91cf27df02751338b301a7e
Gilson José <80110990+gilson789qwe@users.noreply.github.com>
2022-05-03 09:40:58 -0300

    base do controller

---
+++ spring/src/main/java/com/oquedadinheiro/customer/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package com.oquedadinheiro.customer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class CustomerController {
+}
